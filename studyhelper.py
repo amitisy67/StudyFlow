84db82dba905b1d748848635c268f6ee8f1f6f93
amitisy67 <amitisy67@gmail.com>
2026-08-28 18:49:50 +0330

    Add files via upload

---
+++ studyhelper.py
@@ -0,0 +1,28 @@
+study=[]
+import time
+class Pomodoro():
+   def __init__ (self,minutes) :
+      self.minutes=minutes
+      seconds = minutes * 60
+ 
+      while seconds:
+        mins = seconds // 60
+        secs = seconds % 60
+        
+        print(f"{mins:02d}:{secs:02d}", end="\r")
+
+        time.sleep(1)
+        seconds -= 1
+Pomodoro(25)
+print("\nBreak time! 🍵")
+
+study_time = {}
+
+def add_pomodoro(subject, minutes):
+    if subject in study_time:
+                study_time[subject] += minutes
+    else:
+                study_time[subject] = minutes
+
+
+
